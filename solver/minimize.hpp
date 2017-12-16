e7839765f17f91bcccd9bb5c5d3325029e69642f
Monika Roznere <mrozner1@binghamton.edu>
2017-12-16 14:43:34 -0500

    commiting

--- solver/minimize.hpp
+++ solver/minimize.hpp
@@ -1,4 +1,13 @@
 #ifndef MINIMIZE_H
 #define MINIMIZE_H
 
+
+mat computeCost(vector<vector <int>> X, vector<ImageData> y, const mat& theta){
+    mat J;
+    int m;
+    m = y.n_rows;
+    J = arma::arma::sum( (pow(((X * theta) - y), 2)) / (2 * m) );
+    return J;
+}
+
 #endif
